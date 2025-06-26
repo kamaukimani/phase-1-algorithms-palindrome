c559c367dcc635380b913cca13daa0955814349e
kamaukimani <paulkimys285@gmail.com>
2025-06-26 19:38:19 +0300

    update

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversed = word.split('').reverse().join('');
+  return word === reversed;
 }
 
 /* 
-  Add your pseudocode here
+  Pseudocode:
+  - Take input string 'word'
+  - Split the string into an array of characters
+  - Reverse the array
+  - Join the reversed array back into a string
+  - Compare the reversed string to the original
+  - Return true if they are the same, otherwise false
 */
 
 /*
-  Add written explanation of your solution here
+  Explanation:
+  To check if a word is a palindrome, we reverse the word and then compare it to the original word.
+  If both strings are identical, it means the word reads the same forwards and backwards, so we return true.
+  Otherwise, we return false.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +31,21 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
 }
 
 module.exports = isPalindrome;
